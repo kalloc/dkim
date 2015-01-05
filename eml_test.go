55a0b139fbacee9a06f41e25a0717ba55240f741
Nikita Kuznetsov <me@daedalus.ru>
2015-01-05 03:50:00 +0300

    #test: add simple tests

---
+++ eml_test.go
@@ -0,0 +1,27 @@
+package godkim
+
+import (
+    "bufio"
+    "os"
+    "testing"
+)
+
+func Test_Ok(t *testing.T) {
+    var fp *os.File
+
+    fp, _ = os.Open("test_data/valid_1.eml")
+    defer fp.Close()
+    if ParseEml(bufio.NewReader(fp)).Verify() != true {
+        t.Fail()
+    }
+}
+
+func Test_MayBeNotOk(t *testing.T) {
+    var fp *os.File
+
+    fp, _ = os.Open("test_data/invalid_1.eml")
+    defer fp.Close()
+    if ParseEml(bufio.NewReader(fp)).Verify() == true {
+        t.Fail()
+    }
+}
